2c616964124f4848c159d6a9b537566dc7c0339e
Aanya Singh <80dbfc5f-a1fa-489b-afa4-e1acf25bd83a>
2024-03-01 11:44:56 +0000

    dynamic columns

--- webapp/controller/PicklistValueView.controller.js
+++ webapp/controller/PicklistValueView.controller.js
@@ -1,7 +1,6 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
-
+    "sap/ui/model/json/JSONModel",
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
@@ -9,13 +8,12 @@ sap.ui.define([
     function (Controller, JSONModel) {
         "use strict";
 
-        var unixTimestamp
-        var US
-
         return Controller.extend("picklistmanagement.controller.PicklistValueView", {
             onInit: function () {
+                const aLanguages = sap.ui.getCore().getModel("languageModel").getProperty("/languages");
                 this.getView().setModel(new JSONModel({
-                    dateUNIX: undefined
+                    dateUNIX: undefined,
+                    languages: aLanguages,
                 }), "localModel");
                 this.getRouter().getRoute("RouteViewValues").attachPatternMatched(this._onPicklistMatched, this);
             },
@@ -38,142 +36,147 @@ sap.ui.define([
                 const sPath = `/PickListV2(effectiveStartDate=datetime'${effectiveStartDate.replace(".000Z", "")}',id='${picklistId}')`
                 this.getView().bindElement(sPath);
 
+                const aLanguages = this.getModel("localModel").getProperty("/languages");
                 var oTable = this.byId("smarttable");
-                oTable.getBinding("items").refresh()
+                const aCells = aLanguages.map(oLanguage => new sap.m.Input({
+                    id: oLanguage.key,
+                    value: `{${oLanguage.key}}`
+                }));
+                oTable.bindItems("values", new sap.m.ColumnListItem({
+                    cells: aCells
+                }));
             },
 
-            date: function (oEvent) {
-                // Handle the field change event here
-                var effectivedate = oEvent.getParameter("value");
+date: function (oEvent) {
+    // Handle the field change event here
+    var effectivedate = oEvent.getParameter("value");
 
-                this.getModel("localModel").setProperty("/dateUNIX", Math.floor(Date.parse(effectivedate) / 1000));
-            },
+    this.getModel("localModel").setProperty("/dateUNIX", Math.floor(Date.parse(effectivedate) / 1000));
+},
 
-            onTranslationSimple: function () {
-                const oTable = this.getView().byId("smarttable");
-                const oSubmitButton = this.getView().byId("submitButton");
-                oTable.setBusy(true);
-                oSubmitButton.setEnabled(false);
+onTranslationSimple: function () {
+    const oTable = this.getView().byId("smarttable");
+    const oSubmitButton = this.getView().byId("submitButton");
+    oTable.setBusy(true);
+    oSubmitButton.setEnabled(false);
 
-                const aTableItemContexts = oTable.getBinding("items").getContexts();
-                const aPromises = [];
+    const aTableItemContexts = oTable.getBinding("items").getContexts();
+    const aPromises = [];
 
-                aTableItemContexts.forEach((oContext => {
-                    const oRowObject = oContext.getObject();
-                    const aLanguagesToBeTranslated = [];
-                    Object.keys(oRowObject).forEach(key => {
-                        if (key.indexOf("label_") !== 0) return; // not a language label
-                        if (!!oRowObject[key]) return; // already translated
-                        if (key === "label_en_DEBUG") return; // ignore DEBUG
-                        // check for default labels without language
-                        if ((key.match(/_/g) || []).length < 2) return;
+    aTableItemContexts.forEach((oContext => {
+        const oRowObject = oContext.getObject();
+        const aLanguagesToBeTranslated = [];
+        Object.keys(oRowObject).forEach(key => {
+            if (key.indexOf("label_") !== 0) return; // not a language label
+            if (!!oRowObject[key]) return; // already translated
+            if (key === "label_en_DEBUG") return; // ignore DEBUG
+            // check for default labels without language
+            if ((key.match(/_/g) || []).length < 2) return;
 
-                        aLanguagesToBeTranslated.push(key.replace("label_", "").replace("_", "-"));
-                    });
+            aLanguagesToBeTranslated.push(key.replace("label_", "").replace("_", "-"));
+        });
 
-                    const sSource = oRowObject.label_en_US;
-                    aLanguagesToBeTranslated.forEach((sLanguage => {
-                        aPromises.push(
-                            this._translate(sSource, sLanguage)
-                                .then((json => {
-                                    const sTargetProperty = "label_" + sLanguage.replace("-", "_");
-                                    // write translated value back to model
-                                    this.getModel().setProperty(sTargetProperty, json.data, oContext);
-                                }).bind(this)));
-                    }).bind(this));
-                }).bind(this));
+        const sSource = oRowObject.label_en_US;
+        aLanguagesToBeTranslated.forEach((sLanguage => {
+            aPromises.push(
+                this._translate(sSource, sLanguage)
+                    .then((json => {
+                        const sTargetProperty = "label_" + sLanguage.replace("-", "_");
+                        // write translated value back to model
+                        this.getModel().setProperty(sTargetProperty, json.data, oContext);
+                    }).bind(this)));
+        }).bind(this));
+    }).bind(this));
 
-                Promise.all(aPromises).then(() => {
-                    oTable.setBusy(false);
-                    oSubmitButton.setEnabled(true);
-                });
-            },
+    Promise.all(aPromises).then(() => {
+        oTable.setBusy(false);
+        oSubmitButton.setEnabled(true);
+    });
+},
 
-            _translate: function (sSourceText, sTargetLanguageKey) {
-                return fetch(`/api/v1/translation`, {
-                    method: 'POST',
-                    credentials: 'include',
-                    headers: {
-                        'Content-Type': 'application/json' // Set the content type based on your payload
-                    },
-                    body: JSON.stringify({
-                        "sourceLanguage": "en-US",
-                        "targetLanguage": sTargetLanguageKey,
-                        "contentType": "text/plain",
-                        "data": sSourceText
-                    }) // Convert the payload to a JSON string
-                }).then(response => response.json());
-            },
+_translate: function (sSourceText, sTargetLanguageKey) {
+    return fetch(`/api/v1/translation`, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json' // Set the content type based on your payload
+        },
+        body: JSON.stringify({
+            "sourceLanguage": "en-US",
+            "targetLanguage": sTargetLanguageKey,
+            "contentType": "text/plain",
+            "data": sSourceText
+        }) // Convert the payload to a JSON string
+    }).then(response => response.json());
+},
 
-            onSubmit: function () {
-                // check if new effective date is set
-                if (!this.getModel("localModel").getProperty("/dateUNIX")) {
-                    sap.m.MessageToast.show("Please select new effective date first");
-                    return;
-                }
-                const oModel = this.getModel();
-                if (!oModel.hasPendingChanges()) {
-                    sap.m.MessageToast.show("No changes to submit");
-                    return;
-                }
+onSubmit: function () {
+    // check if new effective date is set
+    if (!this.getModel("localModel").getProperty("/dateUNIX")) {
+        sap.m.MessageToast.show("Please select new effective date first");
+        return;
+    }
+    const oModel = this.getModel();
+    if (!oModel.hasPendingChanges()) {
+        sap.m.MessageToast.show("No changes to submit");
+        return;
+    }
 
-                const aRawValueData = this.getView().byId("smarttable").getBinding("items").getContexts().map(context => context.getObject());
-                const allowed = Object.keys(aRawValueData[0]).filter(key => {
-                    if ((key === "status" || key === "externalCode" || key.indexOf("label_") === 0) && key !== "label_localized") {
-                        return true;
-                    } else {
-                        return false;
-                    }
-                });
+    const aRawValueData = this.getView().byId("smarttable").getBinding("items").getContexts().map(context => context.getObject());
+    const allowed = Object.keys(aRawValueData[0]).filter(key => {
+        if ((key === "status" || key === "externalCode" || key.indexOf("label_") === 0) && key !== "label_localized") {
+            return true;
+        } else {
+            return false;
+        }
+    });
 
-                // allow only values in allowlist above
-                const aValueData = aRawValueData.map(oObject => {
-                    let returnObject = {};
-                    Object.keys(oObject).forEach(key => {
-                        if (allowed.includes(key)) {
-                            returnObject[key] = oObject[key];
-                        }
-                    });
-                    return returnObject;
-                });
-
-                const id = this.getView().getBindingContext().getObject().id;
-                const unixTimestamp = this.getModel("localModel").getProperty("/dateUNIX");
-
-                const payload = {
-                    "__metadata": {
-                        "uri": "https://apisalesdemo2.successfactors.eu/odata/v2/PickListV2",
-                        "type": "SFOData.PickListV2"
-                    },
-                    "id": id,
-                    "status": "A",
-                    "effectiveStartDate": "/Date(" + unixTimestamp + "000)/",
-                    "values": {
-                        "results": aValueData
-                    }
-                }
-
-                debugger;
-
-                const requestOptions_create = {
-                    method: 'POST',
-                    credentials: 'include',
-                    headers: {
-                        'Content-Type': 'application/json' // Set the content type based on your payload
-                    },
-                    body: JSON.stringify(payload) // Convert the payload to a JSON string
-                };
-                fetch(`/odata/v2/upsert/`, requestOptions_create)
-                    .then(response => response.json())
-                    .then(json => {
-                        // Handle the response if needed
-                        console.log(json);
-                    })
-                    .catch(error => {
-                        // Handle errors
-                        console.error('Error:', error);
-                    });
+    // allow only values in allowlist above
+    const aValueData = aRawValueData.map(oObject => {
+        let returnObject = {};
+        Object.keys(oObject).forEach(key => {
+            if (allowed.includes(key)) {
+                returnObject[key] = oObject[key];
             }
+        });
+        return returnObject;
+    });
+
+    const id = this.getView().getBindingContext().getObject().id;
+    const unixTimestamp = this.getModel("localModel").getProperty("/dateUNIX");
+
+    const payload = {
+        "__metadata": {
+            "uri": "https://apisalesdemo2.successfactors.eu/odata/v2/PickListV2",
+            "type": "SFOData.PickListV2"
+        },
+        "id": id,
+        "status": "A",
+        "effectiveStartDate": "/Date(" + unixTimestamp + "000)/",
+        "values": {
+            "results": aValueData
+        }
+    }
+
+    const requestOptions_create = {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json' // Set the content type based on your payload
+        },
+        body: JSON.stringify(payload) // Convert the payload to a JSON string
+    };
+    fetch(`/odata/v2/upsert/`, requestOptions_create)
+        .then(response => response.json())
+        .then(json => {
+            // Handle the response if needed
+            console.log(json);
+        })
+        .catch(error => {
+            // Handle errors
+            console.error('Error:', error);
+        });
+}
         });
     });
 
--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,17 +1,19 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-	"sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/ui/model/json/JSONModel",
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, Filter, FilterOperator) {
+    function (Controller, Filter, FilterOperator, JSONModel) {
         "use strict";
 
         return Controller.extend("picklistmanagement.controller.View1", {
             onInit: function () {
-
+                sap.ui.getCore().setModel(new JSONModel({}), "languageModel");
+                this.byId("idPicklistTable").attachEventOnce("updateFinished", this.tableUpdateFinishedOnce, this);
             },
 
             handleItemPress: function (oEvent) {
@@ -42,12 +44,48 @@ sap.ui.define([
                 oBinding.filter(aFilters, "Application");
             },
 
-            tableUpdateStarted: function (oEvent) {
+            tableBusyTrue: function (oEvent) {
                 oEvent.getSource().setBusy(true);
             },
 
-            tableUpdateFinished: function (oEvent) {
+            tableBusyFalse: function (oEvent) {
                 oEvent.getSource().setBusy(false);
+            },
+
+            tableUpdateFinishedOnce: function (oEvent) {
+                // "unbusy" table
+                const oTable = oEvent.getSource();
+                oTable.setBusy(false);
+
+                // check for available langugages in this SFSF system
+                const oOneItemContext = oTable.getBinding("items").getContexts()[0];
+                if (!oOneItemContext) {
+                    // table doesn't have items
+                    return;
+                }
+                this.getView().getModel().read("values", {
+                    context: oOneItemContext,
+                    urlParameters: {
+                        "$top": 1
+                    },
+                    success: function (oData) {
+                        let aLanguages = Object.keys(oData.results[0]).filter(key => {
+                            if (key.indexOf("label_") !== 0) return false;
+                            if (key.indexOf("default") >= 0) return false;
+                            if (key.indexOf("DEBUG") >= 0) return false;
+                            if (key.indexOf("localized") >= 0) return false;
+                            return true;
+                        });
+                        const iUsIndex = aLanguages.indexOf("label_en_US");
+                        const swap = aLanguages[0];
+                        aLanguages[0] = "label_en_US";
+                        aLanguages[iUsIndex] = swap;
+                        const aLanguageObjects = aLanguages.map(sLanguage => ({
+                            key: sLanguage
+                        }));
+                        sap.ui.getCore().getModel("languageModel").setProperty("/languages", aLanguageObjects);
+                    }.bind(this)
+                });
             }
         });
     });
